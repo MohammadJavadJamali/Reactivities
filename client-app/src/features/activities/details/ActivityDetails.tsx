c009a2489071b4adbcced98a88000f51434574b9
mohammadjavad <mjj7661@gmail.com>
2021-04-22 01:26:17 +0430

    Add MobX

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { Button, ButtonGroup, Card, Image } from "semantic-ui-react"
-import { Activity } from "../../../app/models/activity"
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
 
-interface Props{
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props)
+export default function ActivityDetails()
 {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelecteActivity} = activityStore;
+
+    if(!activity) return <LoadingComponent/>;
+
     return(
         <Card fluid>
             <Image src={`/assets/categoryimages/${activity.category}.jpg`} />
@@ -26,7 +27,7 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
 
             <ButtonGroup widths='2'>
                 <Button basic onClick={() => openForm(activity.id)} color='blue' content='Edit'/>
-                <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel'/>
+                <Button onClick={cancelSelecteActivity} basic color='grey' content='Cancel'/>
             </ButtonGroup>
 
             </Card.Content>
